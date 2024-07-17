79691e2061b4bc4403ef92f165f63f9027e57ed7
lxweb01 <chems2.luxtech@gmail.com>
2024-07-16 20:01:39 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,62 @@
+const movies = {
+    'spring-summer-fall-winter-spring': {
+        title: 'Spring Summer Fall Winter... and Spring',
+        img: 'img/kim-ki-duk-spring-1.jpg',
+        director: 'Kim Ki-duk',
+        releaseDate: '19 September 2003',
+        boxOffice: '$9.53 million'
+    },
+    '3-iron': {
+        title: '3-Iron',
+        img: 'img/kim-ki-duk-3-iron.jpg',
+        director: 'Kim Ki-duk',
+        releaseDate: '2004',
+        boxOffice: '$2 million'
+    },
+    'oldboy': {
+        title: 'Oldboy',
+        img: 'img/park-chan-wook-oldboy.jpg',
+        director: 'Park Chan-wook',
+        releaseDate: '2003',
+        boxOffice: '$15 million'
+    },
+    'parasite': {
+        title: 'Parasite',
+        img: 'img/bong-joon-ho-parasite.jpg',
+        director: 'Bong Joon-ho',
+        releaseDate: '2019',
+        boxOffice: '$258 million'
+    },
+    'memories-of-murder': {
+        title: 'Memories of Murder',
+        img: 'img/bong-joon-ho-memories.jpg',
+        director: 'Bong Joon-ho',
+        releaseDate: '2 May 2003',
+        boxOffice: '$20.1 million'
+    },
+    'pieta': {
+        title: 'Pietà',
+        img: 'img/kim-ki-duk-pieta.jpg',
+        director: 'Kim Ki-duk',
+        releaseDate: '3 September 2012',
+        boxOffice: '$9.6 million'
+    },
+    'im-a-cyborg-but-thats-ok': {
+        title: 'I\'m a Cyborg, But That\'s OK',
+        img: 'img/park-chan-wook-cyborg.jpg',
+        director: 'Park Chan-wook',
+        releaseDate: '7 December 2006',
+        boxOffice: '$3.8 million'
+    },
+    'the-handmaiden': {
+        title: 'The Handmaiden',
+        img: 'img/park-chan-wook-handmaiden.jpg',
+        director: 'Park Chan-wook',
+        releaseDate: '1 June 2016',
+        boxOffice: '$38.6 million'
+    }
+};
+
 function searchMovies() {
     const query = document.getElementById('search-bar').value.toLowerCase();
     const movies = document.querySelectorAll('#movie-feed .movie');
@@ -48,8 +107,12 @@ function viewAll() {
     rows.forEach(row => row.style.display = '');
 }
 
-function showMovieDetails(movieName) {
-    // Redirect to a detailed page or show a modal with the movie details
-    // For now, alert the movie name
-    alert('Showing details for ' + movieName);
+function showMovieDetails(movieId) {
+    const movie = movies[movieId];
+    document.getElementById('movie-title').textContent = movie.title;
+    document.getElementById('movie-image').src = movie.img;
+    document.getElementById('movie-director').textContent = movie.director;
+    document.getElementById('movie-release-date').textContent = movie.releaseDate;
+    document.getElementById('movie-box-office').textContent = movie.boxOffice;
+    document.getElementById('movie-details').style.display = 'block';
 }
